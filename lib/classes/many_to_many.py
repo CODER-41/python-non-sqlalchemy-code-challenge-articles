9b74635fc44a22d6c03dd700caa3783b77d8e687
RONNY <mboyaronny41@gmail.com>
2025-11-25 09:28:07 +0300

    Author.py

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -6,10 +6,25 @@ class Article:
         
 class Author:
     def __init__(self, name):
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string")
+        if len(name) == 0:
+            raise ValueError("Name must be longer than 0 characters")
         self.name = name
 
+    @property
+    def name(self):
+        return self.__qualname__
+    
+    @name.setter
+    def name(self, value):
+        if hasattr(self, '_name'):
+            raise  AttributeError("Cannot change name after author is instatiated")
+        self._name = value
+
     def articles(self):
-        pass
+        return[article for article in Article._all if article.auuthor == self]
+        #pass
 
     def magazines(self):
         pass
